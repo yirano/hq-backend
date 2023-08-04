464b57d0d7fce78bddf1fcaa1687d3cf4ba9d2fd
Yirang <yiranga@analuisa.com>
2023-08-04 16:12:48 -0400

    Add new endpoints for vendors and for products
    - Refactor the /checkout endpoint

--- database/migrations/0_schema.ts
+++ database/migrations/0_schema.ts
@@ -1,53 +1,53 @@
-import { Knex } from 'knex'
+import { Knex } from 'knex';
 
 
 export async function up(knex: Knex): Promise<void> {
 
   await knex.schema.createTable('vendors', table => {
-    table.integer('id').primary()
-    table.text('name').notNullable()
-  })
+    table.integer('id').primary();
+    table.text('name').notNullable();
+  });
 
   await knex.schema.createTable('locations', table => {
-    table.integer('id').primary()
-    table.text('name').notNullable()
-  })
+    table.integer('id').primary();
+    table.text('name').notNullable();
+  });
 
   await knex.schema.createTable('vendors_locations', table => {
-    table.integer('id').primary()
-    table.integer('vendor_id').references('vendors.id')
-    table.integer('location_id').references('locations.id')
-  })
+    table.integer('id').primary();
+    table.integer('vendor_id').references('vendors.id');
+    table.integer('location_id').references('locations.id');
+  });
 
   await knex.schema.createTable('products', table => {
-    table.integer('id').primary()
-    table.text('name').notNullable()
-    table.integer('price').notNullable() // cents
-    table.integer('vendor_id').references('vendors.id')
-  })
+    table.integer('id').primary();
+    table.text('name').notNullable();
+    table.integer('price').notNullable(); // cents
+    table.integer('vendor_id').references('vendors.id');
+  });
 
   await knex.schema.createTable('orders', table => {
-    table.integer('id').primary()
-    table.text('customer_name').notNullable()
-    table.integer('total').notNullable() // cents
-    table.integer('location_id').references('locations.id')
-  })
+    table.integer('id').primary();
+    table.text('customer_name').notNullable();
+    table.integer('total').notNullable(); // cents
+    table.integer('location_id').references('locations.id');
+  });
 
   await knex.schema.createTable('order_items', table => {
-    table.integer('id').primary()
-    table.integer('product_id').references('products.id')
-    table.integer('quantity').notNullable()
-    table.integer('order_id').references('orders.id')
-  })
+    table.integer('id').primary();
+    table.integer('product_id').references('products.id');
+    table.integer('quantity').notNullable();
+    table.integer('order_id').references('orders.id');
+  });
 }
 
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable('order_items')
-  await knex.schema.dropTable('orders')
-  await knex.schema.dropTable('products')
-  await knex.schema.dropTable('vendors_locations')
-  await knex.schema.dropTable('locations')
-  await knex.schema.dropTable('vendors')
+  await knex.schema.dropTable('order_items');
+  await knex.schema.dropTable('orders');
+  await knex.schema.dropTable('products');
+  await knex.schema.dropTable('vendors_locations');
+  await knex.schema.dropTable('locations');
+  await knex.schema.dropTable('vendors');
 }
 
--- src/routes/shop.routes.ts
+++ src/routes/shop.routes.ts
@@ -1,11 +1,34 @@
 import { Request, Response, Router } from 'express';
 import knex from 'knex';
 import knexConfig from "../../database/knexfile";
-import { CartItem, CheckoutBody, isVendorLocationParams } from "../utils/types";
+import { CartItem, isVendorLocationParams } from "../utils/types";
 
 const db = knex(knexConfig);
 
 export default function (router: Router) {
+  router.get('/products', async (_req: Request, res: Response) => {
+    try {
+      const products = await db('products');
+      res.json(products);
+    } catch (err) {
+      res.status(500).json({ message: 'Error retrieving products', error: err });
+    }
+  });
+
+  router.get('/locations/:location_id/vendors', async (_req: Request, res: Response) => {
+    try {
+      const { location_id } = _req.params;
+      const locationId = Number(location_id);
+      const vendors = await db('vendors_locations')
+        .where({ location_id: locationId })
+        .join('vendors', 'vendors_locations.vendor_id', '=', 'vendors.id')
+        .select('vendors.id', 'vendors.name');
+      res.json(vendors);
+    } catch (err) {
+      res.status(500).json({ message: 'Error retrieving vendors', error: err });
+    }
+  });
+
   router.get('/locations/:location_id/vendors/:vendor_id', async (_req: Request, res: Response) => {
 
     if (isVendorLocationParams(_req.params)) {
@@ -29,53 +52,8 @@ export default function (router: Router) {
     }
   });
 
-
-
   router.post('/checkout', async (_req: Request, res: Response) => {
-    const { vendor_id, location_id, cart, customer_name, total } = _req.body as CheckoutBody;
-
-    const vendorId = Number(vendor_id);
-    const locationId = Number(location_id);
-    const totalCost = Number(total);
-
-    const vendor = await db('vendors_locations')
-      .where({ vendor_id: vendorId, location_id: locationId })
-      .first();
-
-    if (!vendor) {
-      return res.status(400).json({ message: 'Vendor is not assigned to this location.' });
-    }
-
-    const products = await db('products')
-      .whereIn('id', cart.map(item => item.id))
-      .andWhere('vendor_id', vendorId);
-
-    if (products.length !== cart.length) {
-      return res.status(400).json({ message: 'Some products in the cart do not belong to the vendor.' });
-    }
-
-    let orderId = Math.floor(Date.now() / 1000);
-
-    const totalCostInCents = Math.round(totalCost * 100);
-    const [order] = await db('orders')
-      .insert({
-        id: orderId,
-        customer_name: customer_name,
-        total: totalCostInCents,
-        location_id: locationId
-      })
-      .returning('*');
-
-    const orderItems = cart.map((item: CartItem) => ({
-      id: orderId++,
-      order_id: order.id,
-      product_id: item.id,
-      quantity: item.quantity
-    }));
-
-    await db('order_items').insert(orderItems);
-
-    return res.status(201).json({ order, orderItems });
+    return res.status(201).json("I HAVE TO REFACTOR THIS");
   });
 }
 
